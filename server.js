59ba52f90ab23adfe170ae923a732bf0f392b488
nvvnravi <nvvnravi@gmail.com>
2017-08-05 16:33:48 -0400

    updated to add 3 articles
    updated to add 3 articles

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one', function (req, res) {
+  res.send("Article one requested. Coming Shortly.");
+});
+app.get('/article-two', function (req, res) {
+  res.send("Article two requested. Coming Shortly.");
+});
+app.get('/article-three', function (req, res) {
+  res.send("Article three requested. Coming Shortly.");
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
