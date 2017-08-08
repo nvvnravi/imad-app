264ed18b0e6c2c3583077ebdd25f1e4a71f51dcb
nvvnravi <nvvnravi@yahoo.com>
2017-08-08 05:25:46 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,21 @@
 var myHTMLButton=document.getElementById("myButton");
 myHTMLButton.onClick=function(){
 
-alert("1");
+//alert("1");
+
+var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+	if(request.readyState===XMLHttpRequest.DONE){
+		if(request.status===200){
+			var currentCounter=request.responseText;
+			var spanTagValue=document.getElementById(spanCount);
+			spanTagValue.innerHTML=counter.toString();
+		}
+	}
+};
+
+//Now Make the request
+request.open('GET','/counter');
+
 
 };
