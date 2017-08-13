4dc0b51954755a5b7e4138adcae8889a99a68c05
nvvnravi <nvvnravi@yahoo.com>
2017-08-13 17:49:02 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ res.send(JSON.stringify(names.sort()));
 
 var pool1=new pool(config);
 app.get('/testdb', function (req, res) {
-pool1.query('SELECT * from category', (err, result) => {
+pool1.query('SELECT * from user', (err, result) => {
   if(err){
       res.send("Error in getting records from DB"+err.toString());
   }else{
