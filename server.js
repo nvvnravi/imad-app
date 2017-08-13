5f40eebeb0b404bdf92803633b610fe61adc18bd
nvvnravi <nvvnravi@yahoo.com>
2017-08-13 17:24:56 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var config={
 };
 
 
-//var conString = "pg://nvvnravi:db-nvvnravi-36741@localhost:5432/user";
+var conString = "pg://nvvnravi:db-nvvnravi-36741@localhost:5432/user";
 //var client = new pg.Client(conString);
 //client.connect();
 
