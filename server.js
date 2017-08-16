3b6e8de4f537d779fac003ccd6b7c7d75a291425
nvvnravi <nvvnravi@yahoo.com>
2017-08-16 00:15:30 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -177,7 +177,7 @@ app.post('/create-user',function(req,res){
     //read password from the request body
     var passwordValue=req.body.password;
     //Convert the password into a hashedPassword
-    var hashPassword = hash(passwordValue);
+    var hashPassword = hash(new Buffer(passwordValue, 'binary'));
     //Now insert the user in the table with the passsword
     client.query('INSERT into  user1  (username,password) values ($!,$2)',[username,hashPassword], function(err,res){
      if(err){
