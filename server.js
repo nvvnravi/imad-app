7e36331c74c6aa48f1aaaa947432fbbdbfb48bd1
nvvnravi <nvvnravi@yahoo.com>
2017-08-16 23:53:01 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.get('/', function (req, res) {
 });
 
 function hash(input){
-var salt=crypto.randomBytes(256).toString('hex');
+var salt=crypto.randomBytes(128).toString('hex');
 const key = crypto.pbkdf2Sync(input, 'Mysalt', 100000, 512, 'sha512');
 console.log(key.toString('hex'));  // '3745e48...aa39b34'    
 return key.toString('hex');
