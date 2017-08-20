82c90e10257844b1989186e82b481d54eb7d9c63
nvvnravi <nvvnravi@yahoo.com>
2017-08-20 17:18:44 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -381,11 +381,8 @@ app.post('/login',function(req,res){
               //set the session cookie here
               req.session.auth={userid: result.rows[0].id}
               //send the response
-              //res.send("user successfully logged in!!!!\n");
-              res.sendFile(path.join(__dirname, 'ui', 'landing.html'));
-              
-              
-          }else{
+              res.status(200).send("user successfully logged in!!!!\n");
+               }else{
               res.status(403).send("username/password is invalid.\n");
           }
       }
