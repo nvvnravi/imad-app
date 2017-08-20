9f85a9cf087bbf40af8b0279e2ea159754b05fc5
nvvnravi <nvvnravi@yahoo.com>
2017-08-20 16:48:57 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,14 +163,11 @@ var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
 if(request.readyState===4 && request.status===200){
 var userid=request.responseText;
-    if(userid ==='false'){
-        //var spanTagValue=document.getElementById('spanCount');
-        //spanTagValue.innerHTML=currentCounter.toString();
-        document.getElementById('spanCount').style.display='none';
+    if(userid ==='true'){
+        getCommentHistory(articleId);
     }else{
-     user=parserInt(userid);
-     getCommentHistory(articleId);
-    }
+     
+     }
 }
 };
 //Now Make the request
