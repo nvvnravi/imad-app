33d34db260ade8a4b34f20c78f0e9a4a0d660442
nvvnravi <nvvnravi@yahoo.com>
2017-08-21 20:53:14 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -183,7 +183,8 @@ var responseValue=request.responseText;
 };
 //Now Make the request
 var commentValue=document.getElementById('comment').value;
-alert("comment :"+commentValue+"    articleId:"+articleId+"    userId:"+checkLogin());
+var userId=checkLogin();
+alert("comment :"+commentValue+"    articleId:"+articleId+"    userId:"+userId);
 
 request.open('POST','http://nvvnravi.imad.hasura-app.io/addComment',true);
 request.setRequestHeader('Content-Type','application/json');
