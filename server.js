ec7b589e9e1701b51f235a788368e2696c6acbe8
nvvnravi <nvvnravi@yahoo.com>
2017-08-21 23:31:45 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,7 +146,6 @@ var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
 if(request.readyState=== 4 ){
     if( request.status=== 200){
-    
     // alert("checkLogin :"+request.responseText);
         if(request.responseText === 'false'){
              Document.getElementById('commentArea').style.display='none';
