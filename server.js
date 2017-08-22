8fe1d5f1844f66b2e29fc1125a2f409a861d2f02
nvvnravi <nvvnravi@yahoo.com>
2017-08-21 23:02:37 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,8 +144,8 @@ var articleHTMLTemplate=`
 function checkLogin(){
 var request=new XMLHttpRequest();
 request.onreadystatechange=function(){
-if(request.readyState===4 ){
-    if( request.status===200){
+if(request.readyState=== 4 ){
+    if( request.status=== 200){
     var userid=request.responseText;
      alert("checkLogin :"+userid);
         if(userid === 'false'){
